060d883d056d702bb2a916d1bd7cd25a1af1fc41
Kai <kevin_lin@alumni.brown.edu>
2024-05-22 13:44:03 -0400

    Add authentication service

---
+++ src/services/Api.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export default () => {
+      const axiosInstance = axios.create({
+            baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3001/api/v1',
+            withCredentials: true
+      })
+
+      const token = localStorage.getItem('token')
+      if (token) {
+            axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`
+      }
+
+      axiosInstance.interceptors.response.use(
+            (response) => response,
+            (error) => {
+                  if (error.response && error.response.status === 401) {
+                        localStorage.removeItem('token')
+                        localStorage.removeItem('user')
+                        location.reload()
+                  }
+                  return Promise.reject(error)
+            }
+      )
+
+      return axiosInstance
+}
